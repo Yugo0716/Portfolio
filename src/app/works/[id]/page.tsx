e8fb50d5d1efa8b756147bb8e4a150af701088b0
LAPTOP-46VGUCFU\datemaki716 <kigo1674@outlook.com>
2025-05-12 01:09:43 +0900

    動画をyoutubeのリンクに変更

--- src/app/works/[id]/page.tsx
+++ src/app/works/[id]/page.tsx
@@ -64,7 +64,17 @@ export default function WorkDetailPage(promise: { params: Promise<{ id: string }
         <p><strong>GitHub (Releasesからビルドファイルをダウンロードできます)：</strong> <a href={work.github} className="text-lime-600 underline" target="_blank" rel="noopener noreferrer">{work.github}</a></p>
         <p><strong>開発ツール：</strong> {work.tools}</p>
         <p><strong>プレイ映像：</strong></p>
-        <video controls src={work.video} className="w-full" />
+        <div className="relative w-full pt-[56.25%]">
+            <iframe
+                src={work.video}
+                title="YouTube video player"
+                className="absolute top-0 left-0 w-full h-full rounded"
+                frameBorder="0"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                allowFullScreen
+            ></iframe>
+            </div>
+
       </div>
 
       <div className="h-4" />
