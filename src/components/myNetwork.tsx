5ab6dbe7076a04239cd2e6a986f068055a94c370
Yugo <kigo1674@outlook.com>
2025-06-13 17:08:05 +0900

    ノードをクリックしても説明ポップアップを表示しないノードを用意 ノードの説明文を詳しくした

--- src/components/myNetwork.tsx
+++ src/components/myNetwork.tsx
@@ -7,6 +7,7 @@ interface NodeType {
   id: string | number;
   label: string;
   description?: string;
+  group: string;
 }
 
 interface EdgeType {
@@ -17,20 +18,22 @@ interface EdgeType {
 }
 
 const nodesData: NodeType[] = [
-  { id: 1, label: "ゲーム", description: "私の人生の大部分を占めています。これが無いと生きていけません。" },
-  { id: 2, label: "研究", description: "HCI (Human-Computer Interaction) の分野で、ユーザ体験 (UX) の向上を目指して取り組んでいます。" },
-  { id: 3, label: "音楽", description: "ゲームのBGMを昔からよく聞いていました。ボーカル曲もよく聞きます。" },
-  { id: 4, label: "散歩/サイクリング", description: "暇なときにしばしば目的もなくぶらつくことが好きです。" },
-  { id: 5, label: "ギター", description: "大学3年次にエレキギターを購入しました。" },
-  { id: 6, label: "カラス", description: "大学の研究でカラスの専門家の方との共同研究をしています。" },
-  { id: 7, label: "野球", description: "小学生の頃に始めました。" },
-  { id: 8, label: "合気道", description: "アニメの影響で大学入学時に始めました。" },
-  { id: 9, label: "アニメ", description: "アニメ" },
-  { id: 10, label: "プログラミング", description: "プログラミング" },
-  { id: 11, label: "VR/AR", description: "VR/AR" },
-  { id: 12, label: "グループワーク", description: "グループワーク" },
-  { id: 13, label: "小説", description: "つい最近になって読み始めたため、多く読めているわけではありませんが、魅力を感じます。" },
-  { id: 14, label: "塾講師アルバイト", description: "グループワーク" },
+  { id: 1, label: "ゲーム", description: "私の人生におけるハブです。これが無いと今の私はいません。", group: "desc" },
+  { id: 2, label: "研究", description: "HCI (Human-Computer Interaction) の分野で、ユーザ体験 (UX) の向上を目指して取り組んでいます。" , group: "desc"},
+  { id: 3, label: "音楽", description: "ゲームのBGMを昔からよく聞いていました。ボーカル曲もよく聞きます。" , group: "desc"},
+  { id: 4, label: "散歩/サイクリング", description: "暇なときにしばしば目的もなくぶらつくことが好きです。散歩中に見つけた身近な事柄からアイデアを思いつくこともあります。" , group: "desc"},
+  { id: 5, label: "ギター", description: "大学3年次にエレキギターを購入しました。自らの手で、一般的に広く受け入れられている音楽を奏でられているところが魅力的です。" , group: "desc"},
+  { id: 6, label: "カラス", description: "大学の研究でカラスの専門家の方との共同研究をしています。それまで、毎日のように見ているのにも関わらず、注目したことは無かったカラスの生態について少し分かるようになり、興味を持つようになりました。当たり前のように身近に存在しているものに注目することは難しいですが、新しいコンテンツを作るうえで重要だと思います。" , group: "desc"},
+  { id: 7, label: "野球", description: "小学生～高校生まで軟式野球に取り組んでおりました。野球の経験はただ運動をした以上に私の人との関わり方の姿勢の形成において重要なものであったと感じています。野球を始めるまでの私は内向的な性格でゲームばかりしていました。ですがチームのメンバーは社交的な人が多く、おかげで人と関わることの楽しさに気づきました。今の私もどちらかというと内向的に近い性格ではありますが、より人と関わりたい、人を笑わせたいと思えるようになりました。" , group: "desc"},
+  { id: 8, label: "合気道", description: "大学入学時に始めました。相手の力を受け流すという考え方やその動きに魅了されました。諸事情で2年で辞めてしまいましたが、初めて武道に関わったこともあり有意義な経験でした。" , group: "desc"},
+  { id: 9, label: "アニメ" , group: "nonDesc"},
+  { id: 10, label: "プログラミング", description: "私は、プログラミングを「自分の思い描いたことを実現できるツール」と考えています。" , group: "desc"},
+  { id: 11, label: "VR/AR", description: "私自身はあまり経験はありませんが、現在VRを用いた新しい体験を制作中です。また、将来的に広く普及され、新しいゲーム体験の土台となり得ると考えているため注目しています。" , group: "desc"},
+  { id: 12, label: "人との関わり合い", description: "様々な領域・コミュニティで人と関わってきました。", group: "desc" },
+  { id: 13, label: "小説", description: "つい最近になって読み始めたため、多く読めているわけではありませんが、アニメ・ゲームと比べて、穏やかに進行していく場面を頭の中で想像しながら読み進めていくことに魅力を感じます。" , group: "desc"},
+  { id: 14, label: "塾講師アルバイト", description:"小学生～高校生を相手に個別指導塾で働いておりました。一度学習したことを分かりやすくアウトプットすることは難しいです。その経験を積むことができました。また、私は比較的純粋な（悪く言えば子どもっぽい）性格であると思っているのですが、そのこともあってか生徒の視点で物事を考えることが得意で、教室長や生徒から賞賛の言葉をいただきました。", group: "desc"},
+  { id: 15, label: "古文", group: "nonDesc"},
+  { id: 16, label: "ネットワーク", description:"通信技術に興味があり、OSI参照モデルのそれぞれの階層の役割や通信パケットのしくみ、セキュリティについて勉強していました。", group: "desc"},
 ];
 
 const edgesData: EdgeType[] = [
@@ -42,6 +45,7 @@ const edgesData: EdgeType[] = [
   { from: 1, to: 10 , label:"プログラミングに興味を持つきっかけ", font: {size: 0}},
   { from: 1, to: 11 , label:"新しいゲーム体験", font: {size: 0}},
   { from: 1, to: 12 , label:"複数人プレイ", font: {size: 0}},
+  { from: 1, to: 15 , label:"ゲームのストーリーの元となった物語", font: {size: 0}},
   { from: 2, to: 4 , label:"新しいアイデアの着想", font: {size: 0}},
   { from: 2, to: 6 , label:"カラスを対象とした研究", font: {size: 0}},
   { from: 2, to: 10 , label:"実験システムの実装", font: {size: 0}},
@@ -50,6 +54,7 @@ const edgesData: EdgeType[] = [
   { from: 2, to: 14 , label:"人に論理的に説明する", font: {size: 0}},
   { from: 3, to: 5 , label:"音楽に対する新たな視点", font: {size: 0}},
   { from: 3, to: 13 , label:"小説を題材とした楽曲", font: {size: 0}},
+  { from: 4, to: 6 , label:"カラスの観察", font: {size: 0}},
   { from: 5, to: 8 , label:"脱力感", font: {size: 0}},
   { from: 5, to: 9 , label:"ギターに興味を持つきっかけ", font: {size: 0}},
   { from: 5, to: 12 , label:"バンド活動", font: {size: 0}},
@@ -58,13 +63,14 @@ const edgesData: EdgeType[] = [
   { from: 8, to: 9 , label:"合気道に興味を持つきっかけ", font: {size: 0}},
   { from: 8, to: 12 , label:"部活動運営", font: {size: 0}},
   { from: 10, to: 11 , label:"VR/ARコンテンツの実装", font: {size: 0}},
+  { from: 10, to: 16 , label:"通信処理の実装", font: {size: 0}},
   { from: 12, to: 13 , label:"人との関わり方を考えるきっかけ", font: {size: 0}},
 ];
 
 const MyNetwork = () => {
   const ref = useRef<HTMLDivElement>(null);
-  const [selectedNode, setSelectedNode] = useState<NodeType | null>(null);
-  const [popupPosition, setPopupPosition] = useState<{ x: number; y: number } | null>(null);
+  const [selectedNode, setSelectedNode] = useState<NodeType | null>(null); //クリックで選択されたノード
+  const [popupPosition, setPopupPosition] = useState<{ x: number; y: number } | null>(null); //ノードの説明をするポップアップ
 
   useEffect(() => {
     if (!ref.current) return;
@@ -75,20 +81,9 @@ const MyNetwork = () => {
     const options = {
       nodes: {
         shape: "box",
-        color: {
-          background: "#d9f99d",
-          border: "#65a30d",
-          highlight: {
-            background: "#84cc16",
-            border: "#365314"
-          },
-          hover: {
-            background: "#bef264",
-            border: "#65a30d"
-          }
-        },
+        
         font: {
-          size: 16,
+          size: 20,
           color: "#111827"
         },
         margin: { top: 10, bottom: 10, left: 10, right: 10 }
@@ -107,23 +102,56 @@ const MyNetwork = () => {
       physics: {
         solver: "forceAtlas2Based",
         forceAtlas2Based: {
-          gravitationalConstant: -120,
-          springLength: 200
+          gravitationalConstant: -160,
+          springLength: 220
         },
         stabilization: true
+      },
+      groups: {
+        desc: {
+          color: {
+            background: "#d9f99d",
+            border: "#65a30d",
+            highlight: {
+              background: "#84cc16",
+              border: "#365314"
+            },
+            hover: {
+              background: "#bef264",
+              border: "#65a30d"
+            }
+          }
+        },
+        nonDesc: {
+          color: {
+            background: "#dcdcdc",
+            border: "#aaa",
+            highlight: {
+              background: "#dcdcdc",
+              border: "#aaa",
+            },
+            hover: {
+              background: "#dcdcdc",
+              border: "#aaa",
+            }
+          }
+        }
       }
     };
 
     const network = new Network(ref.current, { nodes, edges }, options);
 
     network.on("click", (params: any) => {
+        //エッジをクリックしても何も起こらないようにする
         if(params.edges.length>0){
           network.unselectAll();
         }
+
+        //descriptionのあるノードをクリックしたら説明ポップアップを表示
         const nodeId = params.nodes[0];
         if (typeof nodeId === 'number' || typeof nodeId === 'string') {
           const node = nodes.get(nodeId);
-          if (node) {
+          if (node && node.description) {
             setSelectedNode(node);
             setPopupPosition({ x: params.pointer.DOM.x, y: params.pointer.DOM.y });
           }
